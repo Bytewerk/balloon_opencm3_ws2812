daedd2dbfe19c72892f4a3d666c207fb1edc8a1b
Hubert Denkmair <hubert@denkmair.de>
2015-08-13 20:56:00 +0200

    hsv fading

--- main.c
+++ main.c
@@ -30,11 +30,12 @@ uint32_t encode_byte(uint8_t b) {
 	uint32_t result = 0;
 	for (int i=0; i<8; i++) {
 		result <<= 3;
-		if (b & 0x01) {
+		if (b & 0x80) {
 			result |= 0b110;
 		} else {
 			result |= 0b100;
 		}
+		b<<=1;
 	}
 	return result;
 }
@@ -54,6 +55,48 @@ void write_rgb(uint32_t spi, uint8_t r, uint8_t g, uint8_t b) {
 	spi_send(spi, 0);
 }
 
+void write_hsv(uint32_t spi, float h, float s, float v) {
+	 float r,g,b;
+
+	 int i;
+	 float f, p, q, t;
+
+	 if( s == 0 ) {
+		 r = g = b = v;
+	 } else {
+
+		 h /= 60;        // sector 0 to 5
+		 i = h;
+		 f = h - i;
+		 p = v * ( 1 - s );
+		 q = v * ( 1 - s * f );
+		 t = v * ( 1 - s * ( 1 - f ) );
+
+		 switch( i )
+			  {
+			case 0:
+			  r = v; g = t; b = p; break;
+
+			case 1:
+			  r = q; g = v; b = p; break;
+
+			case 2:
+			  r = p; g = v; b = t; break;
+
+			case 3:
+			  r = p; g = q; b = v; break;
+
+			case 4:
+			  r = t; g = p; b = v; break;
+
+			default:
+			  r = v; g = p; b = q; break;        // case 5:
+			}
+	 }
+
+	 write_rgb(SPI1, r*255, g*255, b*255);
+}
+
 int main(void)
 {
 	rcc_clock_setup_hse_3v3(&hse_8mhz_3v3[CLOCK_3V3_168MHZ]);
@@ -71,19 +114,14 @@ int main(void)
 	spi_set_dff_8bit(SPI1);
 	spi_enable(SPI1);
 
+	int h;
 	while (1) {
-		write_rgb(SPI1, 255,   0,   0);
-		delay_ms(500);
-		write_rgb(SPI1, 255, 255,   0);
-		delay_ms(500);
-		write_rgb(SPI1, 255, 255, 255);
-		delay_ms(500);
-		write_rgb(SPI1,   0, 255, 255);
-		delay_ms(500);
-		write_rgb(SPI1,   0, 255,   0);
-		delay_ms(500);
-		write_rgb(SPI1,   0,   0, 255);
-		delay_ms(500);
+/*		for (int i=0; i<256; i++) { write_rgb(SPI1, i,0,0); delay_ms(10); }
+		for (int i=0; i<256; i++) { write_rgb(SPI1, 0,i,0); delay_ms(10); }
+		for (int i=0; i<256; i++) { write_rgb(SPI1, 0,0,i); delay_ms(10); } */
+		h = (h+1) % 360;
+		write_hsv(SPI1, h, 1, 1);
+		delay_ms(15);
     }
 
     return 0;
